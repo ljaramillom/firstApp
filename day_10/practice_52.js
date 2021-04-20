35f35405dc5b70cae97b6f7727a39b29c9d9123a
Lizet <jaramillo.lizet@gmail.com>
2021-04-20 02:00:29 +0000

    exercises day 10, item 52

---
+++ day_10/practice_52.js
@@ -0,0 +1,28 @@
+// Escribe una función llamada reducirACero que reciba un número no negativo y retorne el número de pasos que son necesarios para llegar a cero (0) siguiendo estas reglas:
+
+// Si el número actual es par, dividirlo por dos.
+// Si el número actual es impar, restarle 1.
+
+function reducirACero(num) {
+  let count = 0;
+  let n = num;
+
+  for(let i=num;i>=0;i--){
+    if (n % 2 === 0) {
+      count++;
+      n = n / 2;
+    } 
+    if (n % 2 !== 0) {
+      count++;
+      n = n - 1;
+    }
+    if( n === 0) {
+      return count;
+    }
+  }
+}
+
+// cases tests
+console.log(reducirACero(7)) // 5
+console.log(reducirACero(123)) // 12
+console.log(reducirACero(8)) // 4
