749203276aeb7657ad1acccb729f250211939a09
Lizet <jaramillo.lizet@gmail.com>
2021-04-20 02:07:28 +0000

    refactor reducirACero

--- day_10/practice_52.js
+++ day_10/practice_52.js
@@ -26,3 +26,13 @@ function reducirACero(num) {
 console.log(reducirACero(7)) // 5
 console.log(reducirACero(123)) // 12
 console.log(reducirACero(8)) // 4
+
+// refactor
+// function reducirACero(num) {
+//   let count = 0;
+//   while (num > 0) {
+//     num = num % 2 === 0 ? num / 2 : num - 1;
+//     count += 1;
+//   }
+//   return count;
+// }
